2341dfa8b05efd4250c66d5702b252564824463c
catarquaza <iraturner15@gmail.com>
2023-10-24 21:17:44 -0700

    sssssssssssssssssworks

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@ using namespace std;
 #include <iostream>
 #include <cstdlib>
 
-//Floraburn: Grass Fire Shockfin: Water Electric Frostmoth: Ice Bug Shadowmind: Dark Physic Voltbrawler: Electric Fighting Terrafey: Rock Fairy Ironcladore Rock Steel 
-string Mons[10] = {"Floraburn","Shockfin","Frostmoth", "Shadowmind", "Voltbrawler" , "Terrafey" , "Ironcladore", "Spectrake", "Toxiwarrior", "Metatuff"};
-int * picker(int seed)
+// Floraburn: Grass Fire Shockfin: Water Electric Frostmoth: Ice Bug Shadowmind: Dark Physic Voltbrawler: Electric Fighting Terrafey: Rock Fairy Ironcladore Rock Steel
+string Mons[10] = {"Floraburn", "Shockfin", "Frostmoth", "Shadowmind", "Voltbrawler", "Terrafey", "Ironcladore", "Spectrake", "Toxiwarrior", "Metatuff"};
+void makeTeam(int *team)
 {
-    srand(seed);
     int mon[3];
     int Mon1 = rand() % 9;
     int Mon2 = rand() % 9;
@@ -20,9 +19,8 @@ int * picker(int seed)
         {
             Mon2 = rand() % 9;
             cout << Mon2;
-    
             same = true;
-        } 
+        }
         if (Mon1 == Mon3)
         {
             Mon3 = rand() % 9;
@@ -37,33 +35,28 @@ int * picker(int seed)
         {
             break;
         }
-        
     }
-    static int team[3] = {Mon1, Mon2, Mon3};;
-    return team;
+    team[0] = Mon1;
+    team[1] = Mon2;
+    team[2] = Mon3;
 }
-
-
-
 
 int main()
 {
-    int * team1;
-    int * team2;
     srand(time(0));
-    int seed = rand();
-    team1 = picker(seed);
-    seed = rand();
-    team2 = picker(seed);
-    string roster1[3] = {Mons[team1[1]], Mons[team1[2]], Mons[team1[3]]};
-    string roster2[3] = {Mons[team2[1]], Mons[team2[2]], Mons[team2[3]]};
+    int team1[3];
+    int team2[3];
+    makeTeam(team1);
+    makeTeam(team2);
+    string roster1[3] = {Mons[team1[0]], Mons[team1[1]], Mons[team1[2]]};
+    string roster2[3] = {Mons[team2[0]], Mons[team2[1]], Mons[team2[2]]};
     cout << "Team 1: \n";
     for (int i = 0; i < 3; i++)
     {
         cout << roster1[i] << "\n";
     }
     cout << "Team 2: \n";
-     for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         cout << roster2[i] << "\n";
     }
