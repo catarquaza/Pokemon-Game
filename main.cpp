3b49c583666ad3b47a62d1dd3ae8218e0d99cd03
Ira Tuner <102842867+catarquaza@users.noreply.github.com>
2023-10-26 13:50:36 -0700

    ijijpii

--- main.cpp
+++ main.cpp
@@ -10,11 +10,65 @@ string floraburn[] = {"Fire","Grass","80","20","50"};
 string shockfin[] = {"Electric", "Water","60","30","60"};
 string frostmoth[] = {"Ice","Bug","110","30","10"};
 string shadowmind[] = {"Dark","Psycic", "80", "20", "50"};
-string voltbrawler[] = {"Electric","Fighting","100","30","20"};
-string terrafey[] = {"Rock","Fairy","30","50","70"};
-string metatuff[] = {"Metal", "Fighting", "30","100","20"};
-string spectrake[] = {"Dragon", "Ghost", "50", "50", "50"};
-string ironcladore[] = {"Rock", "Steel", "80", "80" , "20"};
+string voltbrawler[] = {"Fighting","Electric","100","30","20"};
+string terrafey[] = {"Fairy" ,"Rock","30","50","70"};
+string metatuff[] = {"Metal" , "Fighting", "30","100","20"};
+string spectrake[] = {"Dragon" , "Ghost", "50", "50", "50"};
+string ironcladore[] = {"Rock" , "Steel", "80", "80" , "20"};
+string toxiwarrior[] = {"Poison" , "Fighting" , "140" , "20" , "20" };
+void makeMoves(int *mve1, int *mve2, int *mve3, int *mve4)
+{
+    int move1 = rand() % 9+1;
+    int move2 = rand() % 9+1;
+    int move3 = rand() % 9+1;
+    int move4 = rand() % 9+1;
+    while (true)
+    {
+        bool same = false;
+        if (move1 == move2)
+        {
+            move2 = rand() % 9;
+            same = true;
+        }
+        if (move2 == move3)
+        {
+            move3 = rand() % 9;
+            same = true;
+        }
+        if (move3 == move4)
+        {
+            move4 = rand() % 9;
+            same = true;
+        }
+        if (move3 == move1)
+        {
+            move4 = rand() % 9;
+            same = true;
+        }
+        if (move1 == move4)
+        {
+            move4 = rand() % 9;
+            same = true;
+        }
+        if (move2 == move4)
+        {
+            move4 = rand() % 9;
+            same = true;
+        }
+        if (same == false)
+        {
+            break;
+        }
+        mve1[0]= {move1 * 10};
+        mve1[1]= {(11-move1)*10};
+        mve2[0]= {move2 * 10};
+        mve2[1]= {(11-move2)*10};
+        mve3[0]= {move3 * 10};
+        mve3[1]= {(11-move3)*10};
+        mve4[0]= {move4 * 10};
+        mve4[1]= {(11-move4)*10};
+    }
+} 
 void makeTeam(int *team)
 {
     int mon[3];
@@ -27,7 +81,6 @@ void makeTeam(int *team)
         if (Mon1 == Mon2)
         {
             Mon2 = rand() % 9;
-            cout << Mon2;
             same = true;
         }
         if (Mon1 == Mon3)
@@ -75,6 +128,10 @@ int main()
         {
             break;
         }
+        else
+        {
+            same = false;
+        }
     };
     string roster1[3] = {Mons[team1[0]], Mons[team1[1]], Mons[team1[2]]};
     string roster2[3] = {Mons[team2[0]], Mons[team2[1]], Mons[team2[2]]};
@@ -88,4 +145,5 @@ int main()
     {
         cout << roster2[i] << "\n";
     }
+
 }
